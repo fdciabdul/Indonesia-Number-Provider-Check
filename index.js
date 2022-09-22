2633b9bfdf9bb58a1c68e54053521639bf411ae9
fdciabdul <abdulmuttaqin456@gmail.com>
2022-09-23 05:45:51 +0700

    add more provider
    By.u and live on

--- index.js
+++ index.js
@@ -12,18 +12,27 @@ const list = {
         "0814", "0815", "0815", "0816", "0855", "0856", "0857", "0858"
     ],
     "Three":[
-        "0894", "0895", "0896", "0897", "0898", "0899"
+        "0895", "0896", "0897", "0898", "0899"
     ],
     "Smartfren":[
-        "0881", "0882", "0883", "0884", "0885", "0886", "0887", "0888", "0888"
-    ]
+        "0881", "0882", "0883", "0884", "0885", "0886", "0887", "0888", "0888","0889"
+    ],
+    "By.u":[
+        "0851"
+    ],
+    "Live One":[
+        "0859"
+    ],
 }
 
 // find the operator
 exports.operator = (number) => {
     let result = 'Unknown';
+
     Object.keys(list).forEach((key) => {
-        if (list[key].includes(number)) {
+        // check if the number is in the list
+        // get the first 4 digits
+        if (list[key].includes(number.substring(0, 4))) {
             result = key;
         }
     });
--- test/operator.test.js
+++ test/operator.test.js
@@ -6,24 +6,27 @@ const operator = require('../index.js').operator;
 // test the function operator
 describe('operator', () => {
     it('should return Telkomsel', () => {
-        expect(operator('0811')).to.equal('Telkomsel');
+        expect(operator('081111111111')).to.equal('Telkomsel');
     });
     it('should return XL', () => {
-        expect(operator('0817')).to.equal('XL');
+        expect(operator('081711111111')).to.equal('XL');
     });
     it('should return Axis', () => {
-        expect(operator('0831')).to.equal('Axis');
+        expect(operator('083111111111')).to.equal('Axis');
     });
     it('should return Indosat', () => {
-        expect(operator('0814')).to.equal('Indosat');
+        expect(operator('081411111111')).to.equal('Indosat');
     });
     it('should return Three', () => {
-        expect(operator('0894')).to.equal('Three');
+        expect(operator('089613222222')).to.equal('Three');
     });
     it('should return Smartfren', () => {
-        expect(operator('0881')).to.equal('Smartfren');
+        expect(operator('088111111111')).to.equal('Smartfren');
+    });
+    it('should return By.u', () => {
+        expect(operator('0851111111111')).to.equal('By.u');
     });
     it('should return Unknown', () => {
-        expect(operator('0810')).to.equal('Unknown');
+        expect(operator('081011111111')).to.equal('Unknown');
     });
 });
